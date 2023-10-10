a22a1e6bc8e709d9a6599d6547e78f20e1f3cd43
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-10 19:33:36 -0400

    remove trailing closing bracket from subnet name

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/vpc.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/vpc.tf
@@ -11,6 +11,6 @@ resource "aws_subnet" "main" {
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "${var.cluster_name}-default-subnet}"
+    Name = "${var.cluster_name}-default-subnet"
   }
 }
