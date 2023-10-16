98c6dabd1e257b6b3f6d33a2b7fb8c583be302fc
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-15 23:58:17 -0400

    separate subnet definition and put in azurerm_subnet

--- kubeadm/v1.26/Azure/Terraform/vnet.tf
+++ kubeadm/v1.26/Azure/Terraform/vnet.tf
@@ -4,8 +4,11 @@ resource "azurerm_virtual_network" "default" {
   resource_group_name = azurerm_resource_group.default.name
   address_space       = ["10.0.0.0/16"]
 
-  subnet {
-    name           = "default"
-    address_prefix = "10.0.1.0/24"
-  }
+}
+
+resource "azurerm_subnet" "default" {
+  name                 = "${var.app_name}-subnet"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
