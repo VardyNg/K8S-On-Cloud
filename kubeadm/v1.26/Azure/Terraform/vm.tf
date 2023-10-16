df53d717c3edf8ebdf48c92086ef27ccf94551e4
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-16 00:05:51 -0400

    add network interface

---
+++ kubeadm/v1.26/Azure/Terraform/vm.tf
@@ -0,0 +1,11 @@
+resource "azurerm_network_interface" "default" {
+  name                = "${var.app_name}-nic"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.default.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
