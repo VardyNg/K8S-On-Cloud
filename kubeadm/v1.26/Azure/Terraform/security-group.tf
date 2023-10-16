7715422580aceec880d4e29a6046898454bb1866
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-15 23:14:15 -0400

    add network security group, add rule to allow inbound to port 22

---
+++ kubeadm/v1.26/Azure/Terraform/security-group.tf
@@ -0,0 +1,17 @@
+resource "azurerm_network_security_group" "default" {
+  name                = "${var.app_name}-nsg"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+
+  security_rule {
+    name                       = "k8s-allow-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
