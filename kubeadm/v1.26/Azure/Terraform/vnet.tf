3c1dcd63a701e933a347be81672c54e81eb7deaf
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-15 23:06:31 -0400

    add virtual network

---
+++ kubeadm/v1.26/Azure/Terraform/vnet.tf
@@ -0,0 +1,11 @@
+resource "azurerm_virtual_network" "default" {
+  name                = "${var.app_name}-network"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "default"
+    address_prefix = "10.0.1.0/24"
+  }
+}
