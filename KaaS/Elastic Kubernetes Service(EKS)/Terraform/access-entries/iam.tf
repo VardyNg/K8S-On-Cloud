b557bb9c53821b67eff0be6edeb542a63ae1e5cb
vardy.ng <wadee@wadeenghoiwa.com>
2024-05-08 17:12:34 -0400

    Add eks:DescribeCluster permission to iam role

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/access-entries/iam.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/access-entries/iam.tf
@@ -5,9 +5,37 @@ resource "aws_iam_role" "admin_role" {
     Statement = [{
       Effect    = "Allow"
       Principal = {
-        AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        AWS = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:root"
       }
       Action    = "sts:AssumeRole"
     }]
   })
+}
+
+resource "aws_iam_policy" "describe_eks_policy" {
+  name        = "${local.name}-describe-eks"
+  path        = "/"
+  description = "Policy to allow describe eks"
+
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "eks:DescribeCluster",
+        ]
+        Effect   = "Allow"
+        Resource = [
+          module.eks.cluster_arn
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "admin_role_policy_attachment" {
+  name       = "DescribeEKSPolicyAttachment"
+  roles      = [aws_iam_role.admin_role.name]
+  policy_arn = aws_iam_policy.describe_eks_policy.arn
 }
