0c493a86f2268d40fd3f85565185186596ae79a6
vardy.ng <wadee@wadeenghoiwa.com>
2024-08-29 13:09:51 -0400

    add access entry

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/pod-mount-ebs-volume/data.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/pod-mount-ebs-volume/data.tf
@@ -1,12 +1,9 @@
-
-
+data "aws_partition" "current" {}
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" {}
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
-
-data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {}
-
 locals {
   name   = basename(path.cwd)
   region = var.region
--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/pod-mount-ebs-volume/eks.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/pod-mount-ebs-volume/eks.tf
@@ -21,7 +21,23 @@ module "eks" {
     }
   }
 
+  access_entries = {
+    # One access entry with a policy associated
+    admin = {
+      kubernetes_groups = []
+      principal_arn     = data.aws_caller_identity.current.arn
 
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type       = "cluster"
+          }
+        }
+      }
+    }
+  }
+  
   cluster_addons = {
     aws-ebs-csi-driver ={
       most_recent = true
