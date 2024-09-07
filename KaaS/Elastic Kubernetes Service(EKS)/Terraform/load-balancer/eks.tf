5ef583ce4774f123b7fbfe94b3e9b9be81ee10f6
vardy.ng <wadee@wadeenghoiwa.com>
2024-09-07 13:52:09 -0400

    upgrade eks module to v20, replace aws-auth by access entry

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/load-balancer/eks.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/load-balancer/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.21"
+  version = "~> 20.0"
 
   cluster_name                   = local.name
   cluster_version                = "1.29"
@@ -21,11 +21,22 @@ module "eks" {
     }
   }
 
-  cluster_addons = {
-    
-  }
+  access_entries = {
+    # One access entry with a policy associated
+    admin = {
+      kubernetes_groups = []
+      principal_arn     = data.aws_caller_identity.current.arn
 
-  manage_aws_auth_configmap = true
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type       = "cluster"
+          }
+        }
+      }
+    }
+  }
 
   tags = local.tags
 }
