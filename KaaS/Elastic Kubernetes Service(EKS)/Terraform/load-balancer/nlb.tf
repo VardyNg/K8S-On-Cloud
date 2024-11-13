b80546adf96db0b7af74b0a852081e28ae5e6d2d
vardy.ng <wadee@wadeenghoiwa.com>
2024-11-13 12:14:23 -0500

    update permission

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/load-balancer/nlb.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/load-balancer/nlb.tf
@@ -4,6 +4,7 @@ resource "kubernetes_service_v1" "nlb-1" {
     annotations = {
       "service.beta.kubernetes.io/aws-load-balancer-type": "external"
       "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type": "ip"
+      "service.beta.kubernetes.io/aws-load-balancer-listener-attributes.TCP-80": "tcp.idle_timeout.seconds=600"
     }
   }
 
