4c76bb0b7c0e6215522ab9c6375dabdc4868a1ff
vardy.ng <wadee@wadeenghoiwa.com>
2024-11-25 10:54:53 -0500

    change name to include random number

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/pod-mount-efs-volume/data.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/pod-mount-efs-volume/data.tf
@@ -4,8 +4,11 @@ data "aws_availability_zones" "available" {}
 data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
+resource "random_id" "random_string" {
+  byte_length = 2
+}
 locals {
-  name   = basename(path.cwd)
+  name   = "${basename(path.cwd)}-${random_id.random_string}"
   region = var.region
 
   vpc_cidr = "10.0.0.0/16"
