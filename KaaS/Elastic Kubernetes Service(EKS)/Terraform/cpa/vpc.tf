a18ea220bee358dee559c8e88853beea62c1ef27
vardy.ng <wadee@wadeenghoiwa.com>
2024-11-30 13:50:45 -0500

    remove unnecessary subnet tags

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/cpa/vpc.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/cpa/vpc.tf
@@ -12,13 +12,5 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-
   tags = local.tags
 }
