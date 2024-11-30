975b87d97e5224bfa34a1d342b54f06ba6ddad59
vardy.ng <wadee@wadeenghoiwa.com>
2024-11-30 14:16:52 -0500

    Add and use variable eks_version to set EKS version

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/nivida/eks.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/nivida/eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name                   = local.name
-  cluster_version                = "1.31"
+  cluster_version                = var.eks_version
   cluster_endpoint_public_access = true
   cluster_endpoint_private_access = true
 
