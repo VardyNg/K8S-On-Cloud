81a59365f759015032698ab3cf5ccd505d33c7c6
vardy.ng <wadee@wadeenghoiwa.com>
2024-12-03 13:06:54 -0500

    Append random string after name

--- KaaS/Elastic Kubernetes Service(EKS)/Terraform/load-balancer/data.tf
+++ KaaS/Elastic Kubernetes Service(EKS)/Terraform/load-balancer/data.tf
@@ -6,8 +6,11 @@ data "aws_eks_cluster_auth" "this" {
 data "aws_caller_identity" "current" {}
 data "aws_availability_zones" "available" {}
 
+resource "random_id" "random_string" {
+  byte_length = 2
+}
 locals {
-  name   = basename(path.cwd)
+  name   = lower("${basename(path.cwd)}-${substr(base64encode(random_id.random_string.b64_url), 0, 3)}")
   region = var.region
 
   vpc_cidr = "10.0.0.0/16"
